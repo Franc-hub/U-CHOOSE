753806a6b8a2fc793870735b3d4567e105e745f0
Franc Cosp <franccospmeyaeso@gmail.com>
2021-06-24 19:09:30 +0000

    some improvents in back but i cant't do the post action

--- src/api/models.py
+++ src/api/models.py
@@ -2,6 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class BaseModel():
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,3 +22,36 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class Student(db.Model,BaseModel):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_logged=db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return '<Student %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fullname":self.fullname,
+            "email": self.email,
+            "is_logged" : self.is_logged
+            # do not serialize the password, its a security breach
+        }
+
+    def db_post(self):        
+        db.session.add(self)
+        db.session.commit()
+
+    def set_with_json(self,json):
+        
+        self.fullname = json["fullname"]
+        self.email = json["email"]
+        self.is_logged= json["is_logged"]
+        self.password = json["password"]
+
+
+    
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Student
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+@api.route("/students",methods=['GET'])
+def all_students():
+    students = Student.get_all()
+    student_dic = []
+    for student in students :
+        student_dic.append(student.serialize())
+    return jsonify(student_dic),200
+
+@api.route('/students',methods=['POST'])
+def create_student ():
+    json = request.get_json()
+    student = Student()
+    student.set_with_json(json)
+    student.db_post()
+    return jsonify(student.serialize()) ,200 
+
